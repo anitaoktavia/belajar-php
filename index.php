7b4778e4b02d8bd8bb7bfac1fd50615d2190e0a4
Anita Oktavia <anitaoktavia641@gmail.com>
2022-10-24 11:17:35 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Hello World! <br>";
+
+$nama ="Anita Oktavia";
+$umur =21;
+
+echo "nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+$namaAdik = "Aulia";
+$umurAdik = 11;
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+
+$selisihUmur = $umur -$umurAdik;
+echo "Selisih usia saya dan adik saya adalah $umur - $umurAdik tahun.<br> selisih umur saya dan adik saya $selisihUmur";
+
+?>
